199baf03b73510b6780947ac0c703b0159645b1d
look-sun <looksun__@naver.com>
2021-01-10 10:00:07 +0900

    django course 18 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
 from accountapp.models import HelloWorld
 
 
@@ -15,6 +17,9 @@ def hello_world(req):
         new_hello_world.text = temp
         new_hello_world.save()
 
-        return render(req, 'accounts/hello_world.html', context={'hello_world_output': new_hello_world})
+        hello_world_list = HelloWorld.objects.all()
+
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
-        return render(req, 'accounts/hello_world.html', context={'text': 'get method!!'})
+        hello_world_list = HelloWorld.objects.all()
+        return render(req, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
